7c7a541bd8ced904185489266437edb644144fd3
Camila4812 <est.maria.clozano@unimilitar.edu.co>
2024-09-30 21:18:41 -0500

    esfera en threeJs

--- AC-26SEPMODELADO_MALLA/main.js
+++ AC-26SEPMODELADO_MALLA/main.js
@@ -13,14 +13,24 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+const geometry1 = new THREE.SphereGeometry(1, 32, 32); // Radio, segmentos en ancho, segmentos en alto
+const material1 = new THREE.MeshBasicMaterial({ color: 0x0077ff, wireframe: true }); // Material básico con wireframe
+const sphere = new THREE.Mesh(geometry1, material1);
 camera.position.z = 5;
+scene.add(sphere);
+
+// Cambiar la posición de la esfera
+sphere.position.x = 2;  // Mover la esfera 2 unidades en el eje X
+sphere.position.y = 1;  // Mover la esfera 1 unidad en el eje Y
+sphere.position.z = -3; // Mover la esfera 3 unidades hacia atrás en el eje Z
 
 function animate() {
 
-
+	sphere.rotation.x += 0.01;
+    sphere.rotation.y+=0.01;
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+	renderer.render( scene,camera);
 
 }
